ecf3d828cc9022099ef15130ba6086c43328c2ac
Santhosh Bomma <SanthoshB@healthewfs.com.au>
2018-06-21 22:04:12 +1000

    add project

--- ARFlightsApi/Controllers/HomeController.cs
+++ ARFlightsApi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARFlightsApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,13 @@ namespace ARFlightsApi.Controllers
     {
         public ActionResult Index()
         {
+            using (var db = new ARFlightContext())
+            {
+                db.Helicopters.Add(new Helicopter() { HelicopterId = 1, HelicopterName = "Port", Origin = "Mel", Destination = "Port Philip", Capacity = 10 });
+                db.SaveChanges();
+
+
+            }
             ViewBag.Title = "Home Page";
 
             return View();
---
+++ ARFlightsApi/Models/Booking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ARFlightsApi.Models
+{
+    public class Helicopter
+    {
+        public int HelicopterId { get; set; }
+        public string HelicopterName { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Capacity { get; set; }
+        public virtual List<Booking> Bookings { get; set; }
+    }
+
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public System.DateTime BookingDate { get; set; }        
+        public int Booked { get; set; }
+        public int HelicopterId { get; set; }
+        public virtual Helicopter Helicopter { get; set; }        
+    }
+
+    public class ARFlightContext : DbContext
+    {
+        public DbSet<Helicopter> Helicopters { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+
+}

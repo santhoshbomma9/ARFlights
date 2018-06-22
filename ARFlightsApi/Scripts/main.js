8dfe49de7e9838b95597e7174ea4084219d17fce
Santhosh Bomma <SanthoshB@healthewfs.com.au>
2018-06-22 10:15:09 +1000

    added api client

--- ARFlightsApi/Scripts/main.js
+++ ARFlightsApi/Scripts/main.js
@@ -3,7 +3,7 @@
 var btn = document.getElementById("checkAvail");
 var booking = document.getElementById("bookingContent");
 var from = document.getElementById("from");
-var tp = document.getElementById("to");
+var to = document.getElementById("to");
 var pax = document.getElementById("pax");
 
 btn.addEventListener("click", function () {
@@ -12,7 +12,8 @@ btn.addEventListener("click", function () {
     //alert(pax.value);
 
     var request = new XMLHttpRequest();
-    request.open("POST", "http://localhost:2523/api/Avail/01-01-2018/2018-2-01/4");
+    //request.open("POST", "http://localhost:2523/api/Avail/01-01-2018/2018-2-01/4");
+    request.open("POST", "http://localhost:2523/api/Avail/" + from.value +"/" + to.value +"/" + pax.value);
     request.onload = function () {
         var Data = JSON.parse(request.responseText);
         renderHTML(Data);
@@ -21,5 +22,9 @@ btn.addEventListener("click", function () {
 });
 
 function renderHTML(data) {
-    booking.innerText = data;
+    var table = $('#bookingTable');
+    $('#bookingTable').find('tbody').empty();
+    for (var i = 0; i < data.length; i++) {
+        table.append("<tr><td >" + data[i].BookingDate + "</td ><td >" + data[i].HelicopterName + "</td ><td >" + data[i].Capacity + "</td ><td >" + data[i].Booked + "</td ><td >" + data[i].Available + "</td ></tr>");
+    }
 }
